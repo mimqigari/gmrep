b0d22c7e4fc91a844cadcb457ceea64586b35027
Mubarak Maigari <tha_rugged@yahoo.com>
2022-04-22 23:15:44 +0100

    first laravel deployment to heroku

--- app/Installer/Controllers/HomeController.php
+++ app/Installer/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends InstallerController
     public function index(RequirementsChecker $checker, AkApi $api)
     {
         $purchase = $api->getAccessCode(config('buzzy.item_id'));
-        $activation = !$purchase || empty($purchase);
+        $activation = empty($purchase);
 
         $requirements = $checker->check(
             config('buzzy.requirements')
